518d55cd7ed669f9ba3c8583b2113be8b954b42e
Leslie Khumalo <leslie.khumalo@icloud.com>
2016-08-14 13:20:42 +0100

    home page update
    added about us scroll to section and rejigged table and table data

--- AngularGotlist/app/welcome/welcome.js
+++ AngularGotlist/app/welcome/welcome.js
@@ -94,6 +94,101 @@ angular.module('myApp.welcome', ['ngRoute'])
             { icon: '', name: 'Cali Roll 3, Location 3, Next Friday', promoter: 'Promoter 3'},
             { icon: '', name: 'Cali Roll 4, Location 4, This Friday', promoter: 'Promoter 4' }
         ];
+
+            $(document).ready(function () {
+        var maxMobileSize = 768; // this value should match the media query
+        var mobileAccordionTableSelector = ".table.mobile-accordion";
+
+
+        // Resize and Reorientation code borrowed and modified from this StackOverflow answer:
+        //		http://stackoverflow.com/a/6603537
+        var previousOrientation = window.orientation;
+
+        var checkOrientation = function() {
+            if(window.orientation !== previousOrientation){
+                previousOrientation = window.orientation;
+                checkScreenSize();
+            }
+        };
+
+        var checkScreenSize = function() {
+            var width = $(window).width();
+
+            if (width < maxMobileSize) {
+                handleMobile();
+            } else {
+                handleDesktop();
+            }
+        }
+
+        window.addEventListener("resize", checkScreenSize, false);
+        window.addEventListener("orientationchange", checkOrientation, false);
+
+        // Android doesn't always fire orientationChange on 180 degree turns
+        setInterval(checkOrientation, 2000);
+
+
+        function handleMobile() {
+            slideUpAllInactive();
+        }
+
+        function handleDesktop() {
+            showRows();
+            addRowHighlighting();
+        }
+
+        function showRows() {
+            $(mobileAccordionTableSelector + " .tr").each(function () {
+                $(this).removeAttr("style");
+            });
+        }
+
+        function addRowHighlighting() {
+            var rows = $(".table .tbody .tr");
+                
+            /* Add a highlighting class on even rows */
+            for (var i = 0; i < rows.length; i++) {
+                if (i % 2 == 1) {
+                    $(rows[i]).addClass("alternate-highlight");
+                }
+            }
+        }
+
+        function slideUpAllInactive() {
+            $(".table .rh").not(".active").each(function() {
+                $(this).next().slideUp();
+            });
+        }
+
+        function handleMobileAccordionTableClick(rowHeader) {
+            var table = $(rowHeader).parents(".table");
+
+            $(table).find(".rh").each(function () {
+                $(this).removeClass("active");
+            });
+
+            slideUpAllInactive();
+
+            var nextRow = $(rowHeader).next();
+            if (!nextRow.is(":visible")) {
+                $(rowHeader).addClass("active");
+                nextRow.slideDown();
+            }
+        }
+
+        $(mobileAccordionTableSelector + " .rh").click(function () {
+            handleMobileAccordionTableClick(this);
+        });
+
+        // Need to run this on first load
+        checkScreenSize();
+    });
+    
+    $(".AboutButton").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".about-us-container").offset().top}, 'slow');
+    });
+    
 }]);
 
 
